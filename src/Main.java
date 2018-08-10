31197ef8a5d00e250b045aefb3ef5e7412737783
hs8 <khoso0521@gmail.com>
2018-08-10 17:33:24 +0900

    update: Tests passed.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         UnixTime ut = new UnixTime();
         int timeNow = (int)(System.currentTimeMillis() / 1000L);
-        ut.setUnixTime(94608000);
+        ut.setUnixTime(-2082844800);
         // ut.setUnixTime(0);
     }
 }
--- src/com/ripplex/unixtime/UnixTime.java
+++ src/com/ripplex/unixtime/UnixTime.java
@@ -17,16 +17,16 @@ public class UnixTime {
     private int month;
     private int day;
     private int[] daysOfMonth;
-    private int isLeapYear;
 
-    private LinkedHashMap<Integer, Integer> dic_year;
+    private LinkedHashMap<Integer, int[]> dic_year;
 
     public static final int SEC_MINUTE = 60;
     public static final int SEC_HOUR = SEC_MINUTE * 60;
     public static final int SEC_DAY = SEC_HOUR * 24;
     public static final int DAYS_OF_YEAR = 365;
+    public static final int YEARS_OFFSET = 369;
 
-    public static final boolean DEBUG_MODE = true;
+    public static final boolean DEBUG_MODE = false;
 
     /**
      * Default constructor
@@ -34,18 +34,17 @@ public class UnixTime {
     public UnixTime() {
 
         this.unixTime = 0;
-        // 閏年計算のため70年分（1970→1900）オフセット
-        this.year = 1900;
+        this.year = 1970 - YEARS_OFFSET;
         this.month = 1;
         this.day = 1;
         this.daysOfMonth = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        // <単位年, 含まれる閏日数>
+        // <単位年, {含まれる日数, カウント}>
         this.dic_year = new LinkedHashMap<>();
-        this.dic_year.put(400, 97);
-        this.dic_year.put(100, 24);
-        this.dic_year.put(4, 1);
-        this.dic_year.put(1, 0);
+        this.dic_year.put(400, new int[]{400 * DAYS_OF_YEAR + 97, 0});
+        this.dic_year.put(100, new int[]{100 * DAYS_OF_YEAR + 24, 0});
+        this.dic_year.put(4, new int[]{4 * DAYS_OF_YEAR + 1, 0});
+        this.dic_year.put(1, new int[]{DAYS_OF_YEAR, 0});
     }
 
     /**
@@ -66,29 +65,30 @@ public class UnixTime {
     public void setUnixTime(int utime) {
         this.unixTime = utime;
 
-        // 閏年計算のため70年分（1970→1900）オフセット（閏年17回）
-        int uday = utime / SEC_DAY;
-        int utime_remain = utime % SEC_DAY;
-        int uday_offset = uday + 70 * DAYS_OF_YEAR + 17;
-        long utime_offset = (long)utime + (70 * DAYS_OF_YEAR + 17) * (long)SEC_DAY;
-        long d_sec = 0;
+        // 閏年計算のため369年分（1970→1601）+ 閏日(89日）オフセット
+        long utime_offset = (long)utime + (YEARS_OFFSET * DAYS_OF_YEAR + 89) * (long)SEC_DAY;
+        long d_sec;
 
         // 年計算
-        for (Map.Entry<Integer, Integer> entry: this.dic_year.entrySet()) {
+        for (Map.Entry<Integer, int[]> entry: this.dic_year.entrySet()) {
             int e_year = entry.getKey();
-            int e_value = entry.getValue();
-            d_sec = (e_year * DAYS_OF_YEAR + e_value) * (long)SEC_DAY;
+            int[] e_value = entry.getValue();
+            d_sec = (e_value[0]) * (long)SEC_DAY;
 
             // 商部分の年数計算
-            int e_year_count = (int)(utime_offset / d_sec);
-            this.year += e_year_count * e_year;
+            e_value[1] = (int)(utime_offset / d_sec);
+
+            // 閏年12/31例外補正
+            if (e_year == 1 && e_value[1] == 4) e_value[1] = 3;
+
+            // 年数加算
+            this.year += e_value[1] * e_year;
 
             // 計算分を減算
-            utime_offset -= d_sec * e_year_count;
+            utime_offset -= d_sec * e_value[1];
 
             // デバッグプリント
-            printDebug(String.valueOf(e_year) + "年計算(" + String.valueOf(e_year_count) + "回) : " +
-                    String.valueOf(this.year) + "年 " + String.valueOf(utime_offset / (double)SEC_DAY) + "日");
+            printDebug(String.valueOf(e_year) + "年計算(" + String.valueOf(e_value[1]) + "回) / ");
         }
 
         // 閏年判定
@@ -96,14 +96,16 @@ public class UnixTime {
             this.daysOfMonth[1] = 29;
         }
 
+        printDebug("残日数 : " + String.valueOf(utime_offset / (double)SEC_DAY) + "日 \n");
+
         // 月計算
-        for (int i = 1; (utime_offset / (d_sec = this.daysOfMonth[i - 1] * SEC_DAY)) > 0; i++) {
+        for (int i = 0; (utime_offset / (d_sec = this.daysOfMonth[i] * SEC_DAY)) > 0; i++) {
             utime_offset -= d_sec;
             this.month++;
         }
 
         // 日計算
-        this.day = (int)(utime_offset / SEC_DAY) + 1;
+        this.day += (int)(utime_offset / SEC_DAY);
     }
 
     /**
@@ -140,6 +142,6 @@ public class UnixTime {
      * @param msg Debug message
      */
     private void printDebug(String msg) {
-        if (DEBUG_MODE) System.out.println(msg);
+        if (DEBUG_MODE) System.out.print(msg);
     }
 }
