1ddc3bf1f402690b6c359792d34dd186b2338f0f
hs8 <khoso0521@gmail.com>
2018-08-24 16:18:35 +0900

    add: Coveralls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,8 @@ import com.ripplex.unixtime.UnixTime;
 public class Main {
 
     public static void main(String[] args) {
-        UnixTime ut = new UnixTime();
-        int timeNow = (int)(System.currentTimeMillis() / 1000L);
-        ut.setUnixTime(-2082844800);
-        // ut.setUnixTime(0);
+        // UnixTime ut = new UnixTime();
+        // int timeNow = (int)(System.currentTimeMillis() / 1000L);
+        // ut.setUnixTime(-2082844800);
     }
 }
